f7e3d1231f0df4e16fc4b3f9a5373147378f4078
luisana <luisanaelenamarin@gmail.com>
2021-02-17 21:07:28 -0400

    detalle de año

--- resources/views/admin/breversRecord.blade.php
+++ resources/views/admin/breversRecord.blade.php
@@ -146,7 +146,7 @@
                                     <div class="position-relative has-icon-left">
                                        <select class="form-control" name="year">
                                             <option value="2020">2020</option>
-                                            <option value="2021">2021</option>
+                                            <option value="2021" selected>2021</option>
                                             <option value="2022">2022</option>
                                             <option value="2023">2023</option>
                                             <option value="2024">2024</option>
